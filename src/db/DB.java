63badc56d22df457e8b4c5ea319c883db2f459fa
Rembau <Rembau@Rembau-PC>
2013-02-26 09:51:30 +0800

    添加getListTest()

--- src/db/DB.java
+++ src/db/DB.java
@@ -1,16 +1,30 @@
 package db;
 
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 public class DB {
-
+	static JdbcTemplate db;
 	public static void main(String[] args) {
 		String xmlPath = "classpath:applicationContext.xml";
 		ApplicationContext ac = new FileSystemXmlApplicationContext(xmlPath);
-		JdbcTemplate db = (JdbcTemplate) ac.getBean("dbTem");
-		int count = db.queryForInt("select count(*) from test;");
-		System.out.println(count);
+		db = (JdbcTemplate) ac.getBean("dbTem");
+		getListTest();
+	}
+	public static void getListTest(){
+		List<Map<String,Object>> list= db.queryForList("select * from test;");
+		for (Map<String, Object> map : list) {
+			Iterator<?> it = map.keySet().iterator();
+			while(it.hasNext()){
+				String str = (String) it.next();
+				System.out.print("key="+str+",value="+map.get(str));
+			}
+			System.out.println();
+		}
 	}
 }
