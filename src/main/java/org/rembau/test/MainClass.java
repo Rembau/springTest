512b255f87b219b8394a3927459c45ed4eb49975
rembau <504229406@qq.com>
2017-03-22 20:56:13 +0800

    完善

--- src/main/java/org/rembau/test/MainClass.java
+++ src/main/java/org/rembau/test/MainClass.java
@@ -9,6 +9,7 @@ public class MainClass {
     public static void main(String args[]) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 new String[]{"classpath:applicationContext.xml"});
+
         TestBean testBean = context.getBean(TestBean.class);
         testBean.handle();
     }
